57f1147712d097340f19387b29f76c45477c8eb4
Niko Sams <ns@vivid-planet.com>
2016-08-22 14:20:37 +0200

    Compatibility with Kwf 4.2

--- Parallax/Kwc/ParallaxImage/Component.php
+++ Parallax/Kwc/ParallaxImage/Component.php
@@ -1,9 +1,9 @@
 <?php
 class Parallax_Kwc_ParallaxImage_Component extends Kwc_Abstract_Composite_Component
 {
-    public static function getSettings()
+    public static function getSettings($param = null)
     {
-        $ret = parent::getSettings();
+        $ret = parent::getSettings($param);
         $ret['generators']['child']['component']['image'] = 'Parallax_Kwc_ParallaxImage_Image_Component';
         $ret['ownModel'] = 'Parallax_Kwc_ParallaxImage_Model';
         $ret['assets']['dep'][] = 'ModernizrTouchevents';
@@ -11,9 +11,9 @@ class Parallax_Kwc_ParallaxImage_Component extends Kwc_Abstract_Composite_Compon
         return $ret;
     }
 
-    public function getTemplateVars()
+    public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer)
     {
-        $ret = parent::getTemplateVars();
+        $ret = parent::getTemplateVars($renderer);
         $ret['parallaxSpeed'] = $this->getRow()->parallax_speed;
         return $ret;
     }
--- Parallax/Kwc/ParallaxImage/Image/Component.php
+++ Parallax/Kwc/ParallaxImage/Image/Component.php
@@ -1,9 +1,9 @@
 <?php
 class Parallax_Kwc_ParallaxImage_Image_Component extends Kwc_Basic_Image_Component
 {
-    public static function getSettings()
+    public static function getSettings($param = null)
     {
-        $ret = parent::getSettings();
+        $ret = parent::getSettings($param);
         $ret['componentName'] = trlKwfStatic('Background Image');
         $ret['dimensions'] = array(
             'fullWidth'=>array(
@@ -17,9 +17,9 @@ class Parallax_Kwc_ParallaxImage_Image_Component extends Kwc_Basic_Image_Compone
         return $ret;
     }
 
-    public function getTemplateVars()
+    public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer)
     {
-        $ret = parent::getTemplateVars();
+        $ret = parent::getTemplateVars($renderer);
         $ret['imageUrl'] = $this->getImageUrl();
         return $ret;
     }
